db789435c0e3be092f504f9ca0e2a61a8388be13
Tomer David Ben Zohar <tomer@wisegroup.co.il>
2018-08-18 21:21:42 +0300

    dist_removing

--- dist/database/migrations/initMigration.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var DB = new MySQL();
-DB.query('create database node_base', function (error, results, fields) {
-  if (error) throw error;
-  console.log('node_base schema has been created');
-});
